9a71a256940b573423b86e959caf9ef09880bb47
sib-139 <sib139@yandex.ru>
2022-07-22 16:35:14 +0300

    complete task6

---
+++ task6/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 0;
+
+Console.WriteLine ("Введите число ");
+string userEnterA = Console.ReadLine();
+a = int.Parse(userEnterA);
+
+        if (a % 2 == 0)
+        {
+        Console.WriteLine("Число четное");
+        }
+        else 
+        {
+        Console.WriteLine("Число нечетное");
+        }
