602474b52cedfd75a02a6ddd6ce4666ace9d9531
sib-139 <sib139@yandex.ru>
2022-07-22 16:26:56 +0300

    complete task2

---
+++ task2/Program.cs
@@ -0,0 +1,21 @@
+﻿  int a = 0;
+  int b = 0;
+
+Console.WriteLine ("Введите число a");
+string userEnterA = Console.ReadLine();
+a = int.Parse(userEnterA);
+
+Console.WriteLine ("Введите число b");
+string userEnterB = Console.ReadLine();
+b = int.Parse(userEnterB);
+
+        if (a > b)
+        {
+            Console.WriteLine("Максимальное число ");
+            Console.WriteLine(a);
+        }
+        else
+        {
+            Console.Write("Максимальное число ");
+            Console.WriteLine(b);
+        }
